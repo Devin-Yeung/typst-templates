9282866201e2a634ae3e280f73e03198f44f191f
Devin-Yeung <53309384+Devin-Yeung@users.noreply.github.com>
2026-01-27 19:44:56 +0800

    feat: add handy command to build documents

--- devenv.nix
+++ devenv.nix
@@ -8,7 +8,13 @@
 
 {
   # https://devenv.sh/packages/
-  packages = [ ];
+  packages = with pkgs; [
+    jq
+    fd
+    ripgrep
+    uutils-findutils # xargs
+    fzf
+  ];
 
   # https://devenv.sh/languages/
   languages.typst.enable = true;
@@ -23,6 +29,26 @@
     echo "Running tests"
     typstyle --version | grep --color=auto "${pkgs.typstyle.version}"
     typst --version | grep --color=auto "${pkgs.typst.version}"
+  '';
+
+  scripts.list-apps.exec = ''
+    nix flake show --json 2>/dev/null \
+    | jq -r '.apps.["${pkgs.stdenv.hostPlatform.system}"] | keys | .[]'
+  '';
+
+  # build all documents
+  scripts.build-all.exec = ''
+    list-apps | rg build | xargs -I {} nix run .#{}
+  '';
+
+  # bd stands for "build document"
+  scripts.bd.exec = ''
+    list-apps | rg build | fzf --bind 'enter:become(nix run .#{})'
+  '';
+
+  # wd stands for "watch document"
+  scripts.wd.exec = ''
+    list-apps | rg watch | fzf --bind 'enter:become(nix run .#{})'
   '';
 
   # https://devenv.sh/git-hooks/
